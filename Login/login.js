2b551f1cb39376c1617ac21b85dffdeb4e68d3f7
Alexandre Pacheco <76924249+AlexandrePacheco-Oz@users.noreply.github.com>
2026-03-27 20:15:18 -0300

    Update login.js to fix sessionStorage redirect loop

--- Login/login.js
+++ Login/login.js
@@ -1,8 +1,9 @@
-   function logar() {
+    function logar() {
       var login = document.getElementById("login").value;
       var senha = document.getElementById("senha").value;
 
       if (login === "admin" && senha === "admin") {
+        sessionStorage.setItem("autenticado", "true");
         location.href = "Buscar.html";
       } else {
         alert("Usuário ou senha incorreto");
